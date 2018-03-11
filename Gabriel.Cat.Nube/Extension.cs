5cb12314e40ab297cc8b86e44693ff50e1879ed7
tetradog <tetradog@gmail.com>
2018-03-11 18:24:30 +0100

    subir archivos

---
+++ Gabriel.Cat.Nube/Extension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gabriel.Cat.Extension
+{
+    public static class Extension
+    {
+
+        public static Uri Append(this Uri uri, params string[] paths)
+        {//source:https://stackoverflow.com/questions/372865/path-combine-for-urls
+            return new Uri(paths.Aggregate(uri.AbsoluteUri, (current, path) => string.Format("{0}/{1}", current.TrimEnd('/'), path.TrimStart('/'))));
+        }
+
+    }
+}
---
+++ Gabriel.Cat.Nube/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gabriel.Cat.Nube
+{
+   public class User
+    {
+        string username;
+        string lenguaje;
+        string email;
+        string uriPhoto;
+        bool isEmailVerified;
+
+        public User(string username, string lenguaje, string email, string uriPhoto, bool isEmailVerified)
+        {
+            this.username = username;
+            this.lenguaje = lenguaje;
+            this.email = email;
+            this.uriPhoto = uriPhoto;
+            this.isEmailVerified = isEmailVerified;
+        }
+
+        public string Username { get => username; set => username = value; }
+        public string Lenguaje { get => lenguaje; set => lenguaje = value; }
+        public string Email { get => email; set => email = value; }
+        public string UriPhoto { get => uriPhoto; set => uriPhoto = value; }
+        public bool IsValidated { get => isEmailVerified; set => isEmailVerified = value; }
+    }
+}
